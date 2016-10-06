28f034bdc3a5403a9184e3617fb121929d0ac6d2
Aniela M Wolkonowski <anielamw@gmail.com>
2016-10-06 12:01:24 -0400

    attempting with class structures

--- import.rb
+++ import.rb
@@ -1,7 +1,13 @@
 # Use this file to import the sales information into the
 # the database.
 
+#  Clancy Wiggum (clancy.wiggum@korning.com),Motorola (MT928534),Chimp Glass,11/13/2012,$795219.34,956306,82547,Monthly
+
 require "pg"
+require "csv"
+require "pry"
+
+system "psql korning < schema.sql"
 
 def db_connection
   begin
@@ -10,4 +16,10 @@ def db_connection
   ensure
     connection.close
   end
+end
+
+@full_invoices = CSV.foreach('sales.csv', headers: true)
+
+@full_invoices.each do |full_invoice|
+  new_invoice = Full_invoice.new(full_invoice)
 end
