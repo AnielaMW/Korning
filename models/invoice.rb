a31750ee07964cdfd3d127f1b2cc716b8b867e2a
Aniela M Wolkonowski <anielamw@gmail.com>
2016-10-06 16:05:06 -0400

    refactored to cleaner working code

--- models/invoice.rb
+++ models/invoice.rb
@@ -1,12 +1,12 @@
 class FullInvoice
-  attr_accessor :employee_info, :customer_info, :product_name,
+  attr_accessor :employee_info, :customer_info, :p_name,
   :sale_date, :sale_amount, :units_sold, :invoice_no,
   :invoice_frequency
 
   def initialize(full_invoice)
     @employee_info = full_invoice[0]
     @customer_info = full_invoice[1]
-    @product_name = full_invoice[2]
+    @p_name = full_invoice[2]
     @sale_date = full_invoice[3]
     @sale_amount = full_invoice[4]
     @units_sold = full_invoice[5]
@@ -14,19 +14,19 @@ class FullInvoice
     @invoice_frequency = full_invoice[7]
   end
 
-  def employee_name
+  def e_name
     @employee_info.split(' (')[0]
   end
 
-  def employee_email
+  def email
     @employee_info.split(' (')[1].chop
   end
 
-  def customer_name
+  def c_name
     @customer_info.split(' (')[0]
   end
 
-  def customer_account_no
+  def account_no
     @customer_info.split(' (')[1].chop
   end
 end
